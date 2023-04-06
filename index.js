d71dc98318c13a5d3634cbc4e4c02d02d9596745
ProfessorFish <67111659+ProfessorFish@users.noreply.github.com>
2023-04-06 20:51:03 +0100

    automatically add .env files to environment

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Variants = require("./src/Classes/Variants/main.js");
 const Standard = require("./src/Classes/Standard/main.js");
+require("dotenv").config();
 
 module.exports = { Variants, Standard }
