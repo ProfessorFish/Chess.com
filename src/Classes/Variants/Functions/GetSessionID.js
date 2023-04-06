2b85e832a5fbe3682933744e9d609ddadb5c66bb
ProfessorFish <67111659+ProfessorFish@users.noreply.github.com>
2023-04-06 20:39:19 +0100

    launch puppeteer in headless

--- src/Classes/Variants/Functions/GetSessionID.js
+++ src/Classes/Variants/Functions/GetSessionID.js
@@ -10,7 +10,7 @@ module.exports = async function GetSessionID() {
     
     const browser = await puppeteer.launch(
         {
-            headless: false
+            headless: true
         }
     );
 
